2aff9966add6b267c413bcac23f2172655d81b99
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-21 20:31:17 -0300

    Update exerc_00.py

--- exerc_00.py
+++ exerc_00.py
@@ -2,7 +2,9 @@ nome = str(input('Qual é seu nome? '))
 if nome == 'Rodrigo':
     print('Que nome bonito!')
 elif nome == 'Israel' or nome == 'Samara' or nome == 'Valdiana':
-    print('seu nome é muito popular no Brasil.')   
+    print('seu nome é muito popular no Brasil.')  
+elif nome in 'Ana Marijalma Carol Luciene':
+    print('Lindo nome do integrante de sua familia')     
 else:
     print('Seu nome é normal.')
 print('Tenha um bom dia, {}!'.format(nome))        
