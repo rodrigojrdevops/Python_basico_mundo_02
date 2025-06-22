cbf71bb1242594f95a4a75590c4682141fc55f49
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-22 15:58:40 -0300

    Update exerc_08.py

--- exerc_08.py
+++ exerc_08.py
@@ -4,4 +4,16 @@ peso = float(input('Qual seu peso? (Kg)'))
 altura = float(input('Qual sua altura? (M)'))
 imc = peso / (altura ** 2)
 print('Seu IMC é {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO!')
+elif imc >= 18.5 and imc < 25:
+    print('Você está no PESO IDEAL!')
+elif imc <= imc < 30:
+    print('Você está com SOBREPESO!')
+elif imc >= 30 and imc < 40:
+    print('Você esta com OBESIDADE!')
+elif imc >= 40:
+    print('Você está com OBESIDADE MÓRBIDA!')
+    
+
 
