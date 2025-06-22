42f3a99a59fa4f1006e4a57e04b205a5a9735184
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-22 14:10:14 -0300

    Update exerc_08.py

--- exerc_08.py
+++ exerc_08.py
@@ -1,2 +1,7 @@
 #codigo reaproveitado#
 
+peso = float(input('Qual seu peso? (Kg)'))
+altura = float(input('Qual sua altura? (M)'))
+imc = peso / (altura ** 2)
+print('Seu IMC é {:.1f}'.format(imc))
+
