a26005231045f594947f804bfd5f1180bce84e05
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-22 12:17:25 -0300

    upgrade

---
+++ exerc_04.py
@@ -0,0 +1,14 @@
+from datetime import date
+atual = date.today().year
+nasc = int(input('Em que ano você nasceu?'))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade == 18:
+    print('Você tem se alistar imediatamente!')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você já deveria ter se alistado há {} anos.'.format(saldo))
+
