c8e078523f48fcfaa1d27a70cd243ed44265fe57
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-23 13:34:31 -0300

    Update exerc_00.py

---
+++ estruras condicionais/exerc_00.py
@@ -0,0 +1,11 @@
+'''for cont in range(0, 11):
+    print(cont)
+print('Acabou!')'''
+#treinando o for#
+
+from time import sleep
+for cont in range(11, -1, -1):
+    print(cont)
+    sleep(0.5)
+print('BUM BUM! POOWW!')
+
