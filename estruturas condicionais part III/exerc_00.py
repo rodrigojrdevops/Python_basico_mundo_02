e874ba966eafdd1fc6c21bd731ae8e66bea897a2
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-24 19:26:52 -0300

    Create exerc_00.py

---
+++ estruturas condicionais part III/exerc_00.py
@@ -0,0 +1,36 @@
+#while True:
+#if
+#if
+#if
+#if
+#break
+
+# Exemplo de uso de estruturas condicionais com o break #
+#ele interrrompe o loop quando a condição é atendida #
+
+'''cont = 1
+while cont <= 10:
+    print(cont, '->', end='')
+    cont += 1
+print('Acabou!')'''
+
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print('A soma dos valores foi {} '.format(s))
+# Fim do programa #
+# A condição de parada é quando o usuário digita 999 #
+# O loop continua até que essa condição seja atendida #
+# O programa soma todos os números digitados pelo usuário #
+# O resultado é exibido ao final do loop #
+# O uso do break permite interromper o loop de forma controlada #
+# O loop while é uma estrutura de repetição que continua enquanto a condição for verdadeira #
+# O uso do break é uma forma de controle de fluxo que permite sair do loop quando necessário
+# O programa é um exemplo simples de como usar o while e o break juntos #
+# O loop while é uma estrutura de repetição que continua enquanto a condição for verdadeira #
+    
+
+
