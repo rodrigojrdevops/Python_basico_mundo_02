b1a8b7de78700fb91a5e324dbcc15baa84ffd332
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-24 12:45:04 -0300

    Create exerc_06.py

---
+++ estruras condicionais/exerc_06.py
@@ -0,0 +1,15 @@
+#Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.#
+núm = int(input('Digite um número: '))
+tot = 0
+for c in range(1, núm + 1):
+    if núm % c == 0:
+        print('\033[33m')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divisível {} vezes'.format(núm, tot))
+if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
