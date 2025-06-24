4e1b24c49a70a4b853e5122fdcfb9c100606c607
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-24 19:34:35 -0300

    Update exerc_00.py

--- estruturas condicionais part III/exerc_00.py
+++ estruturas condicionais part III/exerc_00.py
@@ -14,13 +14,31 @@ while cont <= 10:
     cont += 1
 print('Acabou!')'''
 
-n = s = 0
+'''n = s = 0
 while True:
     n = int(input('Digite um número: '))
     if n == 999:
         break
     s += n
-print('A soma dos valores foi {} '.format(s))
+print('A soma dos valores foi {} '.format(s))'''
+#novo metodo f string#
+
+'''n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma dos valores foi {s}')'''  # Usando f-string para formatação
+
+
+nome = 'José'
+idade = 33
+print(f'O {nome} tem {idade} anos')  # Exemplo de f-string (Python 3.6+)
+print('O {} tem {} anos'.format(nome, idade))  # Exemplo de format() Python 3
+print('O %d anos.' % idade)  # Exemplo de formatação com % (Python 2)
+
+# Fim do exemplo #
 # Fim do programa #
 # A condição de parada é quando o usuário digita 999 #
 # O loop continua até que essa condição seja atendida #
