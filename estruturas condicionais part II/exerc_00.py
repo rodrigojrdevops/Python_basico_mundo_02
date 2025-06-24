2f10174a4a669763179330aaca0b71b53877390c
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-24 14:58:47 -0300

    up

---
+++ estruturas condicionais part II/exerc_00.py
@@ -0,0 +1,12 @@
+#Estruturas de repetição com teste lógico
+#Estudos com o laço de repetição while
+
+#whilenot : passo
+    #pega
+
+''''whilenot:
+if condicao:
+if condicao:
+if condicao:
+    #faz algo'''
+
