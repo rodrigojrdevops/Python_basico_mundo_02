a8565c70d02442d0d04c8099f7d5b736a1a1c14b
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-25 17:02:56 -0300

    Update exerc_05.py

--- estruturas condicionais part III/exerc_05.py
+++ estruturas condicionais part III/exerc_05.py
@@ -3,13 +3,22 @@
 #Quantos produtos custam mais de R$1000.
 #Qual é o nome do produto mais barato.
 
-total = totmil = 0
+total = totmil = menor = cont = 0
+barato =' '
 while True:
     produto = str(input('Nome do produto: '))
     preço = float(input('Preço: R$'))
+    cont += 1
     total += preço
     if preço >1000:
         totmil += 1
+    if cont == 1 or preço < menor:    
+        menor = preço
+        barato = produto
+    else:
+        if preço < menor: 
+            menor = preço  
+            barato = produto 
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
@@ -17,3 +26,7 @@ while True:
         break 
 print('{:-^40}'.format('FIM DO PROGRAMA)'))
 print(f'O total da COMPRA foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
+
